a10b69b85de86d06cea21bd322798809bb5df340
Seva Safris <seva@safris.org>
2019-12-16 23:39:47 +0700

    Use javax.annotation.Generated

--- template/src/main/java/org/openjax/codegen/template/Templates.java
+++ template/src/main/java/org/openjax/codegen/template/Templates.java
@@ -31,7 +31,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.processing.Generated;
+import javax.annotation.Generated;
 
 import org.libj.lang.Strings;
 
